36bc30cc76ebd7bab647201fdd50ac59a847063f
ByeongHyeon Lee <qudgus1984@naver.com>
2022-02-25 00:31:29 +0900

    배경설정

--- pygame_basic/2_background.py
+++ pygame_basic/2_background.py
@@ -10,12 +10,20 @@ screen = pygame.display.set_mode((screen_width, screen_height)) # 설정 값을 
 # 화면 타이틀 설정
 pygame.display.set_caption("Nado Game") # 게임 이름
 
+# 배경 이미지 불러오기
+background = pygame.image.load("C:/Users/mom/Desktop/Gameproject/pygame_basic/background.png")
+
 # 이벤트 루프
 running = True # 게임이 진행중인가?
 while running:
   for event in pygame.event.get(): # pygame이 실행되기 위해 반드시 필요한 문장. 어떤 이벤트가 발생하였는가?
     if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가?
       running = False # 게임이 진행중이 아님
+
+  #screen.fill((0, 0, 255)) # 배경을 rgb로 설정 후 채우는 법
+  screen.blit(background, (0, 0)) # 배경 그리기 첫 번째 0은 x좌표, 두 번째 0은 y좌표
+
+  pygame.display.update() # 게임화면을 다시 그리기!
+
 # pygame 종료
 pygame.quit()
-
